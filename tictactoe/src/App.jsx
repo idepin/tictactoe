43beab3543ed893732ef2213e8f80a62fd541d02
idepin <ervin.4happy@gmail.com>
2026-03-01 21:16:37 +0700

    Refactor xIsNext

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -44,21 +44,19 @@ function Board({ xIsNext, squares, onPlay }) {
 }
 
 export default function Game() {
-  const [xIsNext, setXIsNext] = useState(true);
   const [history, setHistory] = useState([Array(9).fill(null)])
   const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
   const currentSquares = history[currentMove];
 
   function jumpTo(nextMove) {
     setCurrentMove(nextMove);
-    setXIsNext(nextMove % 2 === 0);
   }
 
   function handlePlay(nextSquares) {
     const nextHiscory = [...history.slice(0, currentMove + 1), nextSquares];
     setHistory(nextHiscory);
     setCurrentMove(nextHiscory.length - 1);
-    setXIsNext(!xIsNext);
   }
 
   const moves = history.map((squares, move) => {
