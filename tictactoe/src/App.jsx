4335742421bd6816e63e13332f753d92d599ba5c
idepin <ervin.4happy@gmail.com>
2026-03-01 21:14:37 +0700

    Added time travel feature

--- tictactoe/src/App.jsx
+++ tictactoe/src/App.jsx
@@ -1,9 +1,7 @@
 import { useState } from 'react'
 import Square from './components/Square'
 
-export default function Board() {
-  const [squares, setSquares] = useState(Array(9).fill(null));
-  const [xIsNext, setXIsNext] = useState(true);
+function Board({ xIsNext, squares, onPlay }) {
 
   function handleClick(i) {
     if (squares[i] || calculateWinner(squares)) {
@@ -12,8 +10,8 @@ export default function Board() {
 
     const nextSquares = squares.slice();
     nextSquares[i] = xIsNext ? 'X' : 'O';
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext);
+
+    onPlay(nextSquares);
 
   }
 
@@ -42,6 +40,53 @@ export default function Board() {
       </div>
     </>
 
+  )
+}
+
+export default function Game() {
+  const [xIsNext, setXIsNext] = useState(true);
+  const [history, setHistory] = useState([Array(9).fill(null)])
+  const [currentMove, setCurrentMove] = useState(0);
+  const currentSquares = history[currentMove];
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+    setXIsNext(nextMove % 2 === 0);
+  }
+
+  function handlePlay(nextSquares) {
+    const nextHiscory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHiscory);
+    setCurrentMove(nextHiscory.length - 1);
+    setXIsNext(!xIsNext);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description = '';
+    if (move > 0) {
+      description = 'Go to Move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  })
+
+  return (
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>
+          {moves}
+        </ol>
+      </div>
+    </div>
   )
 }
 
